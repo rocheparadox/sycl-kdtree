ceb058656d1dc10602c05735a296c43b7e126836
rocheparadox <rocheinside@gmail.com>
2024-02-07 21:51:06 -0500

    Commit for the End of Day
    - Nearest neighbour is not in working condition

--- src/khalid_kdtree_sycl.hpp
+++ src/khalid_kdtree_sycl.hpp
@@ -93,9 +93,9 @@ namespace khalid{
     }
 
 
-/*    int tree_level_of_node(int index){
+    int tree_level_of_node(int index){
         return 31 - sycl::clz(index + 1);
-    }*/
+    }
 
     int get_total_level(int vertex_count){
         return 31 - __builtin_clz(vertex_count) + 1;
@@ -179,76 +179,7 @@ namespace khalid{
 
 /*        for(int idx=0; idx<tree.size(); idx++)
             std::cout << device_tree[idx].point << " " << device_tree[idx].lla_index << "\n" << std::flush;*/
-
     }
-
-    /*------------------------- Nearest Neighbor ------------------------*/
-
-    template<typename PointDatatype>
-    void get_nearest_neighbor_kernel(Node<PointDatatype>* device_tree, Point2D<int> query_point, int total_tree_level,
-                                     int vertex_count, sycl::queue device_queue){
-         int *nearest_neighbor_index  = sycl::malloc_shared<int>(1, device_queue);
-        device_queue.submit([&] (sycl::handler &hndlr){
-            hndlr.parallel_for(vertex_count, [=] (sycl::id<1> idx){
-                *nearest_neighbor_index = get_nearest_neighbor(device_tree, query_point, total_tree_level);
-            });
-        });
-
-        device_queue.wait();
-        std::cout << "\nThe nearest Neighbor of " << query_point << " is " << device_tree[*nearest_neighbor_index].point << std::flush;
-    }
-
-    template<typename PointDatatype>
-    void get_nearest_neighbor_kernel(Node<PointDatatype>* modelview_device_tree, PointDatatype* dataview,
-                                     int total_tree_level, int vertex_count, sycl::queue device_queue){
-
-        int* nearest_neighbor_indices = sycl::malloc_shared<int>(vertex_count, device_queue);
-
-        device_queue.submit([&] (sycl::handler &hndlr){
-            hndlr.parallel_for(vertex_count, [=] (sycl::id<1> idx){
-                nearest_neighbor_indices[idx] = get_nearest_neighbor(modelview_device_tree, dataview[idx], total_tree_level);
-            });
-        });
-
-        device_queue.wait();
-/*        for(int idx=0; idx<vertex_count; idx++)
-            std::cout << "\nThe nearest Neighbor of " << dataview[idx] << " is " <<
-                modelview_device_tree[nearest_neighbor_indices[idx]].point << std::flush;*/
-    }
-
-    template<typename PointDatatype>
-    float euclidean_distance(Point3D<PointDatatype> pointa, Point3D<PointDatatype> pointb){
-        return sqrtf(powf(pointa.x - pointb.x, 2) + powf(pointa.y - pointb.y, 2) + powf(pointa.z - pointb.z, 2));
-    }
-
-    template<typename Point>
-    int get_nearest_neighbor(Node<Point>* device_tree, Point query_point, int total_tree_levels, int dimensions) {
-        int level = 0;
-        int nearest_neighbor_index;
-        float nearest_neighbor_distance = -1;
-        int search_index = 0;
-        while (true) {
-            int sorting_dimension = level % dimensions;
-            float euc_distance = euclidean_distance(device_tree[search_index].point, query_point);
-            if (euc_distance < nearest_neighbor_distance || nearest_neighbor_distance == -1) {
-                nearest_neighbor_distance = euc_distance;
-                nearest_neighbor_index = search_index;
-            }
-            // traverse through the tree
-            if (query_point[sorting_dimension] < device_tree[search_index].point[sorting_dimension]) {
-                search_index = khalid::l_child(search_index);
-            } else {
-                search_index = khalid::r_child(search_index);
-            }
-
-            //std::cout << "\n\nlevel: " << level << " , search index: " << search_index << " nearest neighbor index : " << nearest_neighbor_index \
-                            << " nearest n dist: " << nearest_neighbor_distance << " euc distance : " << euc_distance << std::flush;
-            if (++level == total_tree_levels)
-                break;
-        }
-        return nearest_neighbor_index;
-    }
-
 }
 
 
